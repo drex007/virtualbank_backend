ddbece9a1155a91bda959b0df414e6f8e64d19b8
viktor ojemeni <58849181+drex007@users.noreply.github.com>
2022-03-13 20:23:58 +0000

    key config

--- virtualbank/virtualbank/settings.py
+++ virtualbank/virtualbank/settings.py
@@ -22,13 +22,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = os.environ.get("SECRET_KEY")
-SECRET_KEY =  'django-insecure-yw(f%-pkavw@mf--(kyp1v*b5*84qy(=c_c7l92xze*=e2$l-8'
+SECRET_KEY = os.environ.get("SECRET_KEY")
+# SECRET_KEY =  'django-insecure-yw(f%-pkavw@mf--(kyp1v*b5*84qy(=c_c7l92xze*=e2$l-8'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
